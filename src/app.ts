ff060c0004094de7bd8ae2c69e532f36ab78f7cd
Piotr Pustul <piotrpustul1@gmail.com>
2023-08-13 16:00:44 +0100

    interacted with DOM elements, created & used an autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // We are getting access to the HTML elements
@@ -12,13 +29,37 @@ class ProjectInput {
 
     // import the content of "project-input" element and render to the dom
     const importedHTMLContent = document.importNode(
-      this.templateElement.content, // content exist on the HTMLTemplateElement
+      this.templateElement.content,
       true
     );
+    // form element
     this.element = importedHTMLContent.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
 
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     // Call the attach function when the object is created
     this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  // set up an even listener
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   // function that render the content
